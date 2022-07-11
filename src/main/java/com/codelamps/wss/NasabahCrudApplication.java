24e0b8bf171b264a69fb73f63a866b0cccc8242b
Andre Rizaldi Brillianto <78262588+Arzailants@users.noreply.github.com>
2022-07-11 11:14:23 +0700

    Create Web Service Nasabah

--- src/main/java/com/codelamps/wss/NasabahCrudApplication.java
+++ src/main/java/com/codelamps/wss/NasabahCrudApplication.java
@@ -22,24 +22,4 @@ public class NasabahCrudApplication {
         SpringApplication.run(NasabahCrudApplication.class, args);
     }
 
-    @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder()
-                .group("nasabah")
-                .pathsToMatch("/**")
-                .build();
-    }
-
-    @Bean
-    public OpenAPI customOpenAPI(@Value("${application-description}") String appDescription, @Value("${application" +
-            "-version}") String appVersion) {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Nasabah CRUD Application REST API")
-                        .version(appVersion)
-                        .description(appDescription)
-                        .termsOfService("http://swagger.io/terms/")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
-
-    }
 }
